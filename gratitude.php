f038646d5a6652a5492745a50fc8c88c2d5ca369
EnVy <97546360+Rechkalov1@users.noreply.github.com>
2026-01-12 23:37:00 +0200

    Add files via upload

---
+++ gratitude.php
@@ -0,0 +1,303 @@
+<?php
+
+$data = implode("\n", $_POST);
+
+$domain = $_SERVER['HTTP_HOST'];
+$to = "lead@".$domain; 
+$subject = "Lead";
+$message = $data;
+$headers = "From: sender@".$domain;
+
+if(mail($to, $subject, $message, $headers)) {
+    //echo "Письмо успешно отправлено!";
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>CasinoSpinx . Your request has been accepted!</title>
+        <meta property="og:title" content="CasinoSpinx . Your request has been accepted!" />
+        
+        <meta property="og:description" content="CasinoSpinx . Your request has been accepted!" />
+        <meta name="description" content="CasinoSpinx . Your request has been accepted!" />
+        
+
+        <link rel="shortcut icon" href="imagevault/logo-679293fd930bd.png" type="image/x-icon" />
+        <link rel="stylesheet" href="master.css?ver=5.8" />
+       <style>
+         
+       </style> 
+        
+    </head>
+
+    <body>
+
+       
+
+      <div class="device">
+        <div class="container-menu">
+          <button id="burger" class="open-main-nav">
+            <span class="burger"></span>
+          </button>
+          <nav class="main-nav" id="main-nav">
+            <ul>
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <li>
+                <a href="online-games.html">Games</a>
+              </li>
+              <li>
+                <a href="our-journey.html">Who We Are</a>
+              </li>
+              <li>
+                <a href="contact-support.html">Contacts</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="hero-hpp7">
+      <div class="container-hpp7">
+        <div class="box-hero-hpp7">
+         <a href="index.html">
+          <div class="container-img-hpp7">
+            <div class="box-logo-hpp7">
+             <img src="imagevault/logo-679293fd930bd.png" alt="">
+            </div>
+            <p class="site-title-hpp7">CasinoSpinx</p>
+           </div>
+         </a>
+          <h1>CasinoSpinx is your gateway to incomparable fruit adventures, where every spinning reel and every win is a step towards your own success story!</h1>
+        </div>
+      </div>
+      </div>
+      
+
+
+<style>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+	#mainWrapp-txt--detailsog{
+		margin: 0px;
+		padding: 0px;
+		font-family: 'Work Sans', sans-serif;
+		width: 100%;
+		font-size: 18px;
+		padding: 298px 0px;
+	}
+	.bodyClass1-txt--detailsog{
+		background: #e8eaeb;
+		color: #ffffff;
+	}
+	.bodyClass2-txt--detailsog{
+		background: #e2bcb7;
+		color: #fff;
+	}
+	.bodyClass3-txt--detailsog{
+		background: #fff;
+		color: #111;
+	}
+	.wrapage-block-txt--detailsog{
+		background-size: 100%;
+		width: 100%;
+	}
+	.box_main-txt--detailsog{
+		width: 100%;
+		margin: 0 auto;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		align-self: center;
+		align-items: center;
+	}
+	.box_main-txt--detailsog h2{
+		font-size: 24px;
+		padding: 0px 0px 25px;
+	}
+	.box_main-txt--detailsog p{
+		font-weight: 500;
+		font-size: 18px;
+	}
+	p{
+		margin-bottom: 10px;
+	}
+	.mainBlock-txt--detailsog{
+		text-align: center;
+	}
+	.mainBlock-txt--detailsog ul{
+		text-align: start;
+		padding: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+	}
+	.mainBlock-txt--detailsog ul>li span{
+		font-weight: bold;
+	}
+	.mainBlock-txt--detailsog{
+		max-width: 1006px;
+		margin: 0 auto;
+		padding: 40px;
+		background: #7b7d008c;
+		border-radius: 0px;
+	}
+	.mainBlock-txt--detailsog .cBlock-txt--detailsog{
+		text-align: center;
+	}
+
+	.bodyClass3-txt--detailsog .mainBlock-txt--detailsog{
+		background: none;
+		border-top: 2px dashed #e2bcb7;
+		border-bottom: 2px dashed #e2bcb7;
+	}
+	.bodyClass2-txt--detailsog .mainBlock-txt--detailsog{
+		background: #290001;
+		color: #fff !important;
+		box-shadow: 0px 0px 0px #290001;
+	}
+	.bodyClass2-txt--detailsog .mainBlock-txt--detailsog p{
+		color: #fff !important;
+	}
+	.bodyClass1-txt--detailsog .mainBlock-txt--detailsog{
+		background: #01242A;
+		color: #ffffff;
+		border-left: 0px solid #FFD700;
+	}
+	.bodyClass1-txt--detailsog .mainBlock-txt--detailsog p{
+		color: #ffffff !important;
+	}
+	.order-txt--detailsog{
+		font-size: 22px !important;
+	}
+
+	  @media screen and (max-width: 639px) {
+		  .box_main-txt--detailsog p{
+			padding: 0px 15px;
+		  }
+		  .box_main-txt--detailsog h2{
+			  padding: 0px 10px 15px;
+		  }
+		.mainBlock-txt--detailsog{
+			padding: 15px;
+		}
+
+
+	}
+	@media screen and (max-width: 480px) {
+		#mainWrapp-txt--detailsog{
+			height: 100%;
+		}
+	}
+</style>
+<div class="bodyClass2-txt--detailsog" id="mainWrapp-txt--detailsog">
+
+
+	<div class="wrapage-block-txt--detailsog">
+		<div class="box_main-txt--detailsog">
+			<div class="mainBlock-txt--detailsog">
+				<p>Dear Valued Client,</p>
+<p>Your confidence in our services is deeply valued. We've successfully received your inquiry and are currently addressing it with the utmost care.</p>
+<p>Be assured that our expert team is now diligently working to fulfill your request. Striving for excellence in servicing your needs, our primary aim is to deliver outstanding quality and achieve your complete satisfaction.</p>
+<p>Should you have any further questions or need additional support, please feel free to reach out via your preferred method of communication. We cherish each client and are always here to provide assistance.</p>
+<p class="cBlock-txt--detailsog">Warmest regards, The Team!</p>
+			</div>
+		</div>
+	</div>
+
+
+</div>
+
+
+ 
+   
+ <section class="section-footer-hpp7 filterefg--div">
+  <div class="gradient-overlay"></div>
+    <div class="container-hpp7">
+  
+      <div class="polityki-hpp7">
+        <ul class="thumb-polityki-hpp7">
+          
+
+          
+          <li class="">
+            <a class="link-menu-footer-hpp7"  href="safePlay.html">Responsible Social Gaming</a>
+          </li>
+          
+        <li>
+          <a class="link-menu-footer-hpp7"  href="disclaimer.html">Disclaimer</a>
+
+        </li>
+        <li class="">
+          <a class="link-menu-footer-hpp7"  href="privacy.html">Privacy policy</a>
+        </li>
+        <li class="">
+          <a class="link-menu-footer-hpp7"  href="service-terms.html">Terms & Conditions</a>
+        </li>
+        </ul>
+        
+      <div class="thumb-18-footer-hpp7">
+                <h1 class="title-hpp7">Disclaimer</h1>
+            <div class="txt-discl-hpp7">
+              <p>Adult platform 18+. Remember that our games are for fun and not for real winnings. Play with pleasure!</p>
+             </div>
+        <div class="box-img-18-hpp7">
+          <img src="imagevault/img-18-679293fd93130.svg" alt="">
+        </div>
+        
+      </div>
+        
+        <ul class="thumb-polityki-hpp7">
+          <li class="">
+            <a class="link-menu-footer-hpp7"  href="index.html">Home</a>
+          </li>
+          <li class="">
+            <a class="link-menu-footer-hpp7"  href="online-games.html">Games</a>
+          </li>
+          <li class="">
+            <a class="link-menu-footer-hpp7"  href="our-journey.html">Who We Are</a>
+          </li>
+          <li class="">
+            <a class="link-menu-footer-hpp7"  href="contact-support.html">Contacts</a>
+          </li>
+         
+        </ul>
+      </div>
+ 
+   
+      <div class="box-copy-hpp7">
+        <p class="copy-title-hpp7">Copyright ©&nbsp;<span class="color-copy-title-hpp7">CasinoSpinx</span> <span> &nbsp;2025</p>
+          
+    </div>
+    </div>
+   
+   </section> 
+    
+        
+ <script>
+      let burger = document.getElementById('burger'),
+      nav    = document.getElementById('main-nav'),
+      slowmo = document.getElementById('slowmo');
+
+    burger.addEventListener('click', function(e){
+      this.classList.toggle('is-open');
+      nav.classList.toggle('is-open');
+    });
+
+ </script>
+
+
+
+        
+
+</body>
+</html>
